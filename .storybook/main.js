6939091405ec47c7a5de4dfca4bca8555247d381
abrosimov <abrosimov@byndyusoft.com>
2021-08-12 17:38:04 +0500

    move stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
-    stories: ['../src/**/__stories__/**.*', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/preset-create-react-app'
-    ]
+    stories: [
+        '../src/**/__stories__/**.mozaic.*',
+        '../src/**/__stories__/**.fronton.*',
+        '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'
+    ],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app']
 };
