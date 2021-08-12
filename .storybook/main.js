164079ba843ba21fe2e7c053001ed90864526585
abrosimov <abrosimov@byndyusoft.com>
2021-08-12 16:49:40 +0500

    add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+    stories: ['../src/**/__stories__/**.*', '../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/preset-create-react-app'
+    ]
+};
