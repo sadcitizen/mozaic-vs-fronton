fa0fd29dfb0feb4cf5aae041e9fba4a65d7b1b32
abrosimov <abrosimov@byndyusoft.com>
2021-08-13 18:11:40 +0500

    update stories

--- src/Shared/__stories__/Card.mozaic.tsx
+++ src/Shared/__stories__/Card.mozaic.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import { Story } from '@storybook/react';
-import { default as CardComponent } from '../components/Card';
-import CardContainer from '../components/Card/Mozaic/CardContainer';
+import {
+    CardBody,
+    CardContainer,
+    CardContent,
+    CardFigure,
+    CardFooter,
+    CardSubtitle,
+    CardTitle
+} from '../components/Card/Mozaic';
 
 const Template: Story = () => (
     <CardContainer>
-        <CardComponent />
+        <CardFigure
+            src="https://images.unsplash.com/photo-1534889156217-d643df14f14a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=640"
+            alt="Card"
+        />
+        <CardContent>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>There can be a subtitle here.</CardSubtitle>
+            <CardBody>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua.
+                </p>
+            </CardBody>
+            <CardFooter>
+                <a href="/">Default link</a>
+            </CardFooter>
+        </CardContent>
     </CardContainer>
 );
 
---
+++ src/Shared/components/Card/Mozaic/index.ts
@@ -0,0 +1,7 @@
+export { default as CardBody } from './CardBody';
+export { default as CardContainer } from './CardContainer';
+export { default as CardContent } from './CardContent';
+export { default as CardFigure } from './CardFigure';
+export { default as CardFooter } from './CardFooter';
+export { default as CardSubtitle } from './CardSubtitle';
+export { default as CardTitle } from './CardTitle';
