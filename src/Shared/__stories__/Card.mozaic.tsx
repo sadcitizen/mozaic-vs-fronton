47f844d7be2f1a22662ef92771de25867484df62
abrosimov <abrosimov@byndyusoft.com>
2021-08-16 14:02:26 +0500

    add dump mozaic env

--- src/Shared/__stories__/Card.mozaic.tsx
+++ src/Shared/__stories__/Card.mozaic.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Story } from '@storybook/react';
+import MozaicEnv from '../components/MozaicEnv';
 import {
     CardBody,
     CardContainer,
@@ -11,25 +12,27 @@ import {
 } from '../components/Card/Mozaic';
 
 const Template: Story = () => (
-    <CardContainer>
-        <CardFigure
-            src="https://images.unsplash.com/photo-1534889156217-d643df14f14a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=640"
-            alt="Card"
-        />
-        <CardContent>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>There can be a subtitle here.</CardSubtitle>
-            <CardBody>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua.
-                </p>
-            </CardBody>
-            <CardFooter>
-                <a href="/">Default link</a>
-            </CardFooter>
-        </CardContent>
-    </CardContainer>
+    <MozaicEnv>
+        <CardContainer>
+            <CardFigure
+                src="https://images.unsplash.com/photo-1534889156217-d643df14f14a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=640"
+                alt="Card"
+            />
+            <CardContent>
+                <CardTitle>Card title</CardTitle>
+                <CardSubtitle>There can be a subtitle here.</CardSubtitle>
+                <CardBody>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.
+                    </p>
+                </CardBody>
+                <CardFooter>
+                    <a href="/">Default link</a>
+                </CardFooter>
+            </CardContent>
+        </CardContainer>
+    </MozaicEnv>
 );
 
 export const Card = Template.bind({});
---
+++ src/Shared/components/MozaicEnv.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const MozaicEnv: FC = ({ children }): JSX.Element => <>{children}</>;
+
+export default MozaicEnv;
