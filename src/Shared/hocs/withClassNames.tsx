6fd1b7a7c9ff36f0ce5a42da352ca4f5c1fdf561
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-08-20 00:30:07 +0500

    add interface

--- src/Shared/hocs/withClassNames.tsx
+++ src/Shared/hocs/withClassNames.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import { IClassNames } from '../domain/ClassNames';
 
-export default function withClassNames<P, CN>(Component: React.ComponentType<P & { classNames: CN }>, classNames: CN) {
+export default function withClassNames<P, CN>(Component: React.ComponentType<P & IClassNames<CN>>, classNames: CN) {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,react/display-name
     return (props: P) => <Component classNames={classNames} {...props} />;
 }
