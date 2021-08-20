aab403336eddd3dede7e36e82332a9d17ed4d5c1
Eugene Abrosimov <sadcitizen@yandex.ru>
2021-08-20 09:02:18 +0500

    add typing

--- src/Shared/hocs/withClassNames.tsx
+++ src/Shared/hocs/withClassNames.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { ComponentType, ReactNode } from 'react';
 import { IClassNames } from '../domain/ClassNames';
 
-export default function withClassNames<P, CN>(Component: React.ComponentType<P & IClassNames<CN>>, classNames: CN) {
-    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,react/display-name
-    return (props: P) => <Component classNames={classNames} {...props} />;
+export default function withClassNames<P, CN>(Component: ComponentType<P & IClassNames<CN>>, classNames: CN) {
+    // eslint-disable-next-line react/display-name
+    return (props: P): ReactNode => <Component {...props} classNames={classNames} />;
 }
